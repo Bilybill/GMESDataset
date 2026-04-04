338be9115f67c1e1d05be02f65a82ab66a454a87
Bilybill <yonghao-16@mails.tsinghua.edu.cn>
2026-04-04 18:25:28 +0800

    Add more file splits in run_large_model_generation.sh

--- run_large_scale_model_generation.sh
+++ run_large_scale_model_generation.sh
@@ -11,12 +11,57 @@ SEED_OFFSET=0
 
 cd /home/wangyh/Project/GMESUni/GMESDataset
 
+# python build_pretraining_dataset.py \
+#   --stage models \
+#   --velocity-root "$VELOCITY_ROOT" \
+#   --sample-root "$SAMPLE_ROOT" \
+#   --output-root "$OUTPUT_ROOT" \
+#   --split-dirs train-river \
+#   --anomaly-types igneous_swarm igneous_stock gas hydrate brine_fault massive_sulfide salt_dome sediment_basement serpentinized \
+#   --anomaly-selection-mode random_one \
+#   --variants-per-model "$VARIANTS_PER_MODEL" \
+#   --seed-offset "$SEED_OFFSET" \
+#   --anomaly-random-config "$ANOMALY_RANDOM_CONFIG" \
+#   --resume \
+#   --stop-on-error
+
+
 python build_pretraining_dataset.py \
   --stage models \
   --velocity-root "$VELOCITY_ROOT" \
   --sample-root "$SAMPLE_ROOT" \
   --output-root "$OUTPUT_ROOT" \
-  --split-dirs train-river \
+  --split-dirs train-choas \
+  --anomaly-types igneous_swarm igneous_stock gas hydrate brine_fault massive_sulfide salt_dome sediment_basement serpentinized \
+  --anomaly-selection-mode random_one \
+  --variants-per-model "$VARIANTS_PER_MODEL" \
+  --seed-offset "$SEED_OFFSET" \
+  --anomaly-random-config "$ANOMALY_RANDOM_CONFIG" \
+  --resume \
+  --stop-on-error
+
+  
+python build_pretraining_dataset.py \
+  --stage models \
+  --velocity-root "$VELOCITY_ROOT" \
+  --sample-root "$SAMPLE_ROOT" \
+  --output-root "$OUTPUT_ROOT" \
+  --split-dirs test-river \
+  --anomaly-types igneous_swarm igneous_stock gas hydrate brine_fault massive_sulfide salt_dome sediment_basement serpentinized \
+  --anomaly-selection-mode random_one \
+  --variants-per-model "$VARIANTS_PER_MODEL" \
+  --seed-offset "$SEED_OFFSET" \
+  --anomaly-random-config "$ANOMALY_RANDOM_CONFIG" \
+  --resume \
+  --stop-on-error
+
+  
+python build_pretraining_dataset.py \
+  --stage models \
+  --velocity-root "$VELOCITY_ROOT" \
+  --sample-root "$SAMPLE_ROOT" \
+  --output-root "$OUTPUT_ROOT" \
+  --split-dirs test-choas \
   --anomaly-types igneous_swarm igneous_stock gas hydrate brine_fault massive_sulfide salt_dome sediment_basement serpentinized \
   --anomaly-selection-mode random_one \
   --variants-per-model "$VARIANTS_PER_MODEL" \
